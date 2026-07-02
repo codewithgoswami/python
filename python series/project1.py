65e03d8b20684d88e0d9a304dc4730da37bfdd56
kuldeep goswami <goswamikuldeep138@gmail.com>
2026-07-02 08:10:34 +0530

    project1

--- python series/project1.py
+++ python series/project1.py
@@ -1,17 +1,17 @@
-# Stone Paper Scissors Game
+# Stone, Paper, Scissors Game
 
 '''1 for stone
--1 for paper
+-1 for paper 
 0 for scissors'''
 
-import random
+'''import random
 
 computer = random.choice([1, -1, 0])
 youstr = input("Enter your choice: ")
 youDict = {"stone":1, "paper":-1, "scissors":0}
 reverseDict = {1:"stone", -1:"paper", 0:"scissors"}
 
-you = youDict[youstr]
+you = youDict.get(youstr)
 
 print(f"you choose {reverseDict[you]}\ncomputer is choosing... {reverseDict[computer]}")
 
@@ -38,4 +38,5 @@ else:
         print("You Loose! Computer wins!")
 
     else:
-        print("Invalid input! Please enter stone, paper or scissors.")
+        print("Invalid input! Please enter stone, paper or scissors'''.") 
+ 
